b714951dae26db69add09e36743207aba6edd621
Reynan M Maglimolo <marcialrey21@gmail.com>
2026-04-27 18:36:30 +0800

    Update settings to use dynamic Brevo SMTP variables

--- crop_backend/settings.py
+++ crop_backend/settings.py
@@ -157,13 +157,14 @@ REST_FRAMEWORK = {
 }
 
 # --- ENTERPRISE EMAIL CONFIGURATION (SMTP) ---
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-# Grabs credentials safely from your .env file so hackers can't see them
 import os
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_USE_SSL = False  # Typically, you use either TLS or SSL, not both
+
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
