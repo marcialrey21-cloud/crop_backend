47a6bb96c02cac9ac6b79eb0884943ec9510facd
Reynan M Maglimolo <marcialrey21@gmail.com>
2026-04-27 17:18:16 +0800

    Switch to Port 465 SSL for Gmail timeout fix

--- crop_backend/settings.py
+++ crop_backend/settings.py
@@ -159,8 +159,9 @@ REST_FRAMEWORK = {
 # --- ENTERPRISE EMAIL CONFIGURATION (SMTP) ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 # Grabs credentials safely from your .env file so hackers can't see them
 import os
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
